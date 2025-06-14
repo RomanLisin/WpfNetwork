3c106f3a858942c63504df28f9bcf3ec03b84bf7
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-06-14 13:12:03 +1000

    Ok Worked

--- IPcalc/MainWindow.xaml.cs
+++ IPcalc/MainWindow.xaml.cs
@@ -1,51 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// MainWindow.xaml.cs
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;  // fo using IPAddress
 
-namespace IPcalc
+namespace IPCalculatorWPF
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-		private void IpChanged(object sender, RoutedEventArgs e)
-		{
-			if (IPAddress.TryParse(IpTextBox.Text, out var ip))
-			{
-				byte first = ip.GetAddressBytes()[0];
-				if (first < 128) Spin.Value = 8;
-				else if (first < 192) Spin.Value = 16;
-				else if (first < 224) Spin.Value = 24;
-			}
-		}
-		private void IpAddressChanged(object sender, RoutedEventArgs e)
-		{
-		
-		}
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrefixBox.Value = 24;
+            UpdateMaskFromPrefix(24);
         }
-		private void OnCancelClicked(object sender, RoutedEventArgs e)
-		{
-			Close();
-		}
+
+        private void OnIpAddressChanged(object sender, RoutedEventArgs e)
+        {
+            if (IPAddress.TryParse(IpTextBox.Text, out var ip))
+            {
+                byte first = ip.GetAddressBytes()[0];
+                if (first < 128) PrefixBox.Value = 8;
+                else if (first < 192) PrefixBox.Value = 16;
+                else if (first < 224) PrefixBox.Value = 24;
+            }
+        }
+
+        private void PrefixBox_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (PrefixBox.Value.HasValue)
+                UpdateMaskFromPrefix(PrefixBox.Value.Value);
+        }
+
+        private void MaskTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (IPAddress.TryParse(MaskTextBox.Text, out var mask))
+            {
+                var bytes = mask.GetAddressBytes();
+                uint m = BitConverter.ToUInt32(bytes.Reverse().ToArray(), 0);
+                int prefix = 0;
+                while ((m & 0x80000000) != 0)
+                {
+                    prefix++;
+                    m <<= 1;
+                }
+                PrefixBox.Value = prefix;
+            }
+        }
+
+        private void UpdateMaskFromPrefix(int prefix)
+        {
+            uint mask = prefix == 0 ? 0 : uint.MaxValue << (32 - prefix);
+            var bytes = BitConverter.GetBytes(mask).Reverse().ToArray();
+            MaskTextBox.Text = string.Join(".", bytes);
+        }
+
+        private void OnOkClicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!IPAddress.TryParse(IpTextBox.Text, out var ip))
+                    throw new Exception("Некорректный IP-адрес.");
+
+                if (!IPAddress.TryParse(MaskTextBox.Text, out var mask))
+                    throw new Exception("Некорректная маска.");
+
+                var ipBytes = ip.GetAddressBytes();
+                var maskBytes = mask.GetAddressBytes();
+                var networkBytes = new byte[4];
+
+                for (int i = 0; i < 4; i++)
+                    networkBytes[i] = (byte)(ipBytes[i] & maskBytes[i]);
+
+                var broadcastBytes = new byte[4];
+                for (int i = 0; i < 4; i++)
+                    broadcastBytes[i] = (byte)(ipBytes[i] | ~maskBytes[i]);
+
+                uint totalIps = (uint)Math.Pow(2, 32 - (PrefixBox.Value ?? 24));
+                uint usableIps = totalIps > 2 ? totalIps - 2 : 0;
+
+                InfoTextBlock.Text =
+                    $"Адрес сети:\t\t{new IPAddress(networkBytes)}\n" +
+                    $"Широковещательный адрес:\t{new IPAddress(broadcastBytes)}\n" +
+                    $"Количество IP-адресов:\t{totalIps}\n" +
+                    $"Количество узлов:\t\t{usableIps}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OnCancelClicked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
